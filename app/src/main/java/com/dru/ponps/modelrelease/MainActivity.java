ca63f728770f20312c1d8c05e9f417a69a8713e1
Ponps <anthonyponcet9@gmail.com>
2017-01-03 19:36:07 +0100

    Init android project

---
+++ app/src/main/java/com/dru/ponps/modelrelease/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dru.ponps.modelrelease;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
